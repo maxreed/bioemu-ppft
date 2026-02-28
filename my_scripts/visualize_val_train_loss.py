fa3cec083748ea9958e4f1cfef8bae8d935bdec1
maxreed <reed.maximilian@gmail.com>
2026-02-27 20:07:55 -0500

    Adding plotting against epoch

--- my_scripts/visualize_val_train_loss.py
+++ my_scripts/visualize_val_train_loss.py
@@ -9,15 +9,15 @@ history = run.history()
 fig, ax = plt.subplots(figsize=(8, 4))
 
 # Drop NaNs per column before plotting so matplotlib has actual points to connect
-train_loss = history[["_epoch", "train/loss"]].dropna()
-val_loss = history[["_epoch", "val/loss"]].dropna()
+train = history[["_step", "epoch", "train/loss"]].dropna()
+val = history[["_step", "epoch", "val/loss"]].dropna()
 
 # We'll add a smoothed loss for val loss to make it easier to interpret
-less_epochs = val_loss["_epoch"][1:-1]
+less_epochs = val_loss["epoch"][1:-1]
 smoothed_val_loss = (val_loss["val/loss"][2:]+val_loss["val/loss"][1:-1]+val_loss["val/loss"][:-2])/3.
 
-ax.plot(train_loss["_epoch"], train_loss["train/loss"], label="Train loss")
-ax.plot(val_loss["_epoch"], val_loss["val/loss"], label="Val loss")
+ax.plot(train_loss["epoch"], train_loss["train/loss"], label="Train loss")
+ax.plot(val_loss["epoch"], val_loss["val/loss"], label="Val loss")
 ax.plot(less_epochs, smoothed_val_loss, label="Val loss (Smoothed)", linewidth=4)
 
 ax.set_xlabel("Epoch")
