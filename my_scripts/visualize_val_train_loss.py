1a41c3846a1450a08cd72703b06fe268fe47eef6
maxreed <reed.maximilian@gmail.com>
2026-02-27 20:04:29 -0500

    Improving visualization, adding smoothing to validation loss

--- my_scripts/visualize_val_train_loss.py
+++ my_scripts/visualize_val_train_loss.py
@@ -9,13 +9,18 @@ history = run.history()
 fig, ax = plt.subplots(figsize=(8, 4))
 
 # Drop NaNs per column before plotting so matplotlib has actual points to connect
-train_loss = history[["_step", "train/loss"]].dropna()
-val_loss = history[["_step", "val/loss"]].dropna()
+train_loss = history[["_epoch", "train/loss"]].dropna()
+val_loss = history[["_epoch", "val/loss"]].dropna()
 
-ax.plot(train_loss["_step"], train_loss["train/loss"], label="Train loss")
-ax.plot(val_loss["_step"], val_loss["val/loss"], label="Val loss")
+# We'll add a smoothed loss for val loss to make it easier to interpret
+less_epochs = val_loss["_epoch"][1:-1]
+smoothed_val_loss = (val_loss["val/loss"][2:]+val_loss["val/loss"][1:-1]+val_loss["val/loss"][:-2])/3.
 
-ax.set_xlabel("Step")
+ax.plot(train_loss["_epoch"], train_loss["train/loss"], label="Train loss")
+ax.plot(val_loss["_epoch"], val_loss["val/loss"], label="Val loss")
+ax.plot(less_epochs, smoothed_val_loss, label="Val loss (Smoothed)", linewidth=4)
+
+ax.set_xlabel("Epoch")
 ax.set_ylabel("Loss")
 ax.set_title("Foldedness fine-tuning loss")
 ax.legend()
