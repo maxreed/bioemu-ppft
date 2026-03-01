fb2c9703c6a30c96948b299fae541c1f08cd3819
maxreed <reed.maximilian@gmail.com>
2026-02-28 20:11:07 -0500

    Scripts for testing the finetuned v5 model.

--- test_validation_set.sh
+++ test_validation_set.sh
@@ -2,9 +2,9 @@
 #SBATCH --account=def-awoolley
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1       # we actually don't need a GPU for this one
-#SBATCH --mem=16G               # memory (per node)
+#SBATCH --mem=200M               # memory (per node)
 #SBATCH --time=0-01:00          # time (DD-HH:MM)
-#SBATCH --output=test_validation_set.log
+#SBATCH --output=test_validation_set_finetuned_v5.log
 
 module load StdEnv/2023 python/3.11 cuda/12.2 gcc/12.3
 source bioemu_env/bin/activate
@@ -12,5 +12,5 @@ source bioemu_env/bin/activate
 MUTANTS=("Y71F" "Y4F" "V160I" "V103L" "R149K" "M72V" "M72F" "L79M" "L6M" "L53V" "L159V" "L113I" "K104A" "F90Y" "A155C" "A130V")
 
 for s in "${MUTANTS[@]}"; do
-  python my_scripts/new_loss.py --xtc outputs/bioemu1p1_KRAS/KRAS_$s/samples.xtc --pdb outputs/bioemu1p1_KRAS/KRAS_$s/topology.pdb --name KRAS_$s
+  python my_scripts/new_loss.py --xtc outputs/v5_validation/KRAS_$s/samples.xtc --pdb outputs/v5_validation/KRAS_$s/topology.pdb --name KRAS_$s
 done
